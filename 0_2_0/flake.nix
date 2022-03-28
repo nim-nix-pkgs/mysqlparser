9a13308031055b5036dfd6291d38772652739336
hugosenari <hugosenari@gmail.com>
2022-03-27 21:35:19 -0300

    chore: re index mysqlparser

--- 0_2_0/flake.nix
+++ 0_2_0/flake.nix
@@ -11,6 +11,7 @@
   inputs.src-mysqlparser-0_2_0.ref   = "refs/tags/0.2.0";
   inputs.src-mysqlparser-0_2_0.owner = "tulayang";
   inputs.src-mysqlparser-0_2_0.repo  = "mysqlparser";
+  inputs.src-mysqlparser-0_2_0.dir   = "";
   inputs.src-mysqlparser-0_2_0.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
